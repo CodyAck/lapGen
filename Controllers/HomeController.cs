510d04b4f7f648848ce3374407519a65f4038c72
CodyAck <42305077+CodyAck@users.noreply.github.com>
2022-06-19 21:07:31 -0400

    Finished

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,6 +9,8 @@ using lapGen.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 using lapGen.Objects;
+using lapGen.Services;
+using lapGen.Utils;
 
 namespace lapGen.Controllers;
 
@@ -16,45 +18,38 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private readonly DataCon dbContext;
+    private readonly IRecordsService recService;
+    private readonly IDriverService driverSerivce;
+    private readonly ICarService carService;
+    private readonly ILapService lapService;
     public bool LapGenToggle = false;
-    //private readonly ITest test;
+    private LapUtilitys UtilitysRef = new LapUtilitys();
 
-    public HomeController(ILogger<HomeController> logger, DataCon dbContext)
+    public HomeController(ILogger<HomeController> logger, DataCon dbContext,
+    IRecordsService recService,IDriverService driverSerivce,ICarService carService, ILapService lapService )
     {
         _logger = logger;
         this.dbContext = dbContext;
-        //this.test = test;
+        this.recService = recService;
+        this.driverSerivce = driverSerivce;
+        this.carService = carService;
+        this.lapService = lapService;
     }
 
     public IActionResult Crud()
     {
         var cars = dbContext.Cars.ToList();
         var drivers = dbContext.Drivers.ToList();
-        //HttpContext.Session.SetObjectAsJson("Drivers",drivers);
-        var laps = dbContext.Laps.ToList();
-        //var raceReport = dbContext.Races.Include(r => r.track.laps).ToList();
+        var laps = dbContext.Laps
+        .Include(l =>l.records).ThenInclude(r =>r.car)
+        .Include(l=>l.records).ThenInclude(r=>r.driver)
+        .ToList();
+        var records = dbContext.Records.ToList();
         var viewMod = new GeneralDataViewModel();
-        foreach(Lap item in laps)
-        {
-            LapsDisplay temp = new LapsDisplay();
-            temp.lap_I_D = item.ID;
-            temp.lapTime = item.recordedLapTime;
-            var tempSelectString = dbContext.Cars.Where(car => car.ID == item.carID).Select(car => car.name).ToList();
-            temp.carName = tempSelectString[0].ToString();
-
-            var tempSelectInt = dbContext.Cars.Where(car => car.ID == item.carID).Select(car => car.number).ToList();
-            temp.carNum =  Convert.ToInt32(tempSelectInt[0]);
-
-            tempSelectString = dbContext.Drivers.Where(driver => driver.ID == item.recordedDriverID).Select(driver => driver.name).ToList();
-            temp.recordedDriverName = tempSelectString[0].ToString();
-            viewMod.displayLaps.Add(temp);
-        }
         viewMod.cars = cars;
         viewMod.drivers = drivers;
         viewMod.laps = laps;
-        
-        //viewMod.driverNames = viewMod.GetDrivers_IE();
-        
+        viewMod.records = records;
         return View(viewMod);
     }
 
@@ -63,110 +58,43 @@ public class HomeController : Controller
         var laps = dbContext.Laps.ToList();
         var cars = dbContext.Cars.ToList();
         var drivers = dbContext.Drivers.ToList();
+        var records = dbContext.Records.ToList();
         var viewMod = new LapGenViewModel();
         viewMod.laps = laps;
         viewMod.cars = cars;
         viewMod.drivers =drivers;
+        viewMod.records = records;
         return View(viewMod);
     }
     public IActionResult Reporting()
     {
         var laps = dbContext.Laps.ToList();
-        var cars = dbContext.Cars.ToList();
-        var drivers = dbContext.Drivers.ToList();
+        var cars = dbContext.Cars.Where(c => c.active).ToList();
+        var drivers = dbContext.Drivers.Where(d => d.active).ToList();
+        var records = dbContext.Records.ToList();
         var viewMod = new ReportingViewModel();
         viewMod.laps = laps;
         viewMod.cars = cars;
         viewMod.drivers =drivers;
+        viewMod.records = records;
         return View(viewMod);
     }
-
-    public IActionResult Car()
-    {
-        return View();
-    }
-
-    public string CalculateLapTime(string currentTime, int inc)
-    {
-        //time is hours:Minutes:Seconds
-        string[] data = currentTime.Split(":");
-        int hours = Convert.ToInt32(data[0]);
-        int minutes = Convert.ToInt32(data[1]);
-        int seconds = Convert.ToInt32(data[2]);
-        int nanoSeconds = Convert.ToInt32(data[3]);
-        string returnString = "";
-
-        seconds +=inc;
-        if(seconds > 60)
-        {
-            seconds = 0;
-            minutes++;
-        }
-        if(minutes > 60)
-        {
-            minutes = 0;
-            hours++;
-        }
-        
-        if(hours < 10)
-        returnString += returnString + "0" + hours +":";
-        else
-        returnString += returnString + hours +":";
-
-        if(minutes < 10)
-        returnString += returnString + "0" + minutes +":";
-        else
-        returnString += returnString + minutes +":";
-
-        if(seconds < 10)
-        returnString += returnString + "0" + seconds +":";
-        else
-        returnString += returnString + seconds +":";
-
-
-        return returnString;
-        
-    }
-
-    public async Task<IActionResult> GenLaps(string ddlCarVal, string ddlDriverVal){
+    public void GenLaps(){
         LapGenToggle = true;
         string previusLapTime = "00:00:00";
         int lapCountTime = 1;
-        while (LapGenToggle)
-        {
-            int lapnumber = 1;
-            Lap newLap = new Lap();
-            using (dbContext)
-            {
-                int selectedDriver = Convert.ToInt32(ddlCarVal);
-                int selectedCar = Convert.ToInt32(ddlDriverVal);
-                newLap.carID = selectedCar;
-                newLap.recordedDriverID = selectedDriver;
-                newLap.lapNumber = lapnumber;
-                previusLapTime = CalculateLapTime(previusLapTime,lapCountTime);
-                newLap.recordedLapTime = previusLapTime;
-                dbContext.Laps.Add(newLap);
-                await dbContext.SaveChangesAsync();
-
-                lapnumber++;
-
-            }
-        }
-        return RedirectToAction("Index");
     }
 
     public void StopLaps(){
         LapGenToggle = false;
     }
-
     [HttpPost]
-    #region Create
+#region Create
         public async Task<IActionResult> CreateDriver(GeneralDataViewModel model)
         {
             using (dbContext)
             {
-                dbContext.Drivers.Add(model.newDriver);
-                await dbContext.SaveChangesAsync();
+                driverSerivce.CreateDriver(model.newDriver.name,0);
                 return RedirectToAction("Crud");
             }
         }
@@ -174,135 +102,57 @@ public class HomeController : Controller
         {
             using (dbContext)
             {
-                model.newCar.SetAsStockCar();
-                int selectedValue = Convert.ToInt32(Request.Form["ddl_driver"]);
-                List<Driver> dbRef_Drivers = dbContext.Drivers.ToList();
-                foreach (Driver item in dbRef_Drivers)
-                {
-                    if(item.ID == selectedValue)
-                    {
-                        model.newCar.currentDriver = item.ID;
-                        model.newCar.possibleDrivers.Add(item);
-                    }
-                } 
-                dbContext.Cars.Add(model.newCar);
-                await dbContext.SaveChangesAsync();
+                carService.CreateCar(model.newCar.number,model.newCar.name);
                 return RedirectToAction("Crud");
             }
-        }
-        
-        
+        }     
         public async Task<IActionResult> CreateLap(GeneralDataViewModel model)
         {
             using (dbContext)
             {
-                int selectedDriver = Convert.ToInt32(Request.Form["ddl_drivers"]);
-                int selectedCar = Convert.ToInt32(Request.Form["ddl_cars"]);
-                int selectedRace = Convert.ToInt32(Request.Form["ddl_races"]);
-                List<Driver> dbRef_Drivers = dbContext.Drivers.ToList();
-                List<Car> dbRef_Cars = dbContext.Cars.ToList();
-                model.newLap.carID = selectedCar;
-                model.newLap.recordedDriverID = selectedDriver;
-
-
-                // foreach (Driver item in dbRef_Drivers)
-                // {
-                //     if(item.ID == selectedDriver)
-                //     {
-                //         item.name = model.newDriver.name;
-                //         item.rank = model.newDriver.rank;
-                        
-                        
-                //     }
-                // }
-                
-                dbContext.Laps.Add(model.newLap);
-                await dbContext.SaveChangesAsync();
+                lapService.CreateLap(model.newLap.lapNumber);
                 return RedirectToAction("Crud");
             }
         }
-    #endregion
-    #region Read
-    
-    #endregion
-    #region Update
+        public async Task<IActionResult> CreateRecord(GeneralDataViewModel model)
+        {
+            int selectedDriver = Convert.ToInt32(Request.Form["ddl_drivers"]);
+            int selectedCar = Convert.ToInt32(Request.Form["ddl_cars"]);
+            recService.CreateRecord(UtilitysRef.ConvertTimeInputToSeconds(model.timeIN),selectedDriver,selectedCar,model.newLap.lapNumber);
+            return RedirectToAction("Crud");
+        }
+#endregion
+#region Update
         public async Task<IActionResult> UpdateDriver(GeneralDataViewModel model)
         {
-            using (dbContext)
-            {
-                int selectedValue = Convert.ToInt32(Request.Form["ddl_drivers"]);
-                List<Driver> dbRef_Drivers = dbContext.Drivers.ToList();
-
-                foreach (Driver item in dbRef_Drivers)
-                {
-                    if(item.ID == selectedValue)
-                    {
-                        item.name = model.newDriver.name;
-                        item.rank = model.newDriver.rank;
-                        dbContext.Drivers.Update(item);
-                        await dbContext.SaveChangesAsync();
-                        
-                    }
-                }
-                
-                
-                return RedirectToAction("Crud");
-            }
+            int selectedValue = Convert.ToInt32(Request.Form["ddl_drivers"]);
+            driverSerivce.UpdateDriver(selectedValue, model.newDriver.name, model.newDriver.rank);
+            return RedirectToAction("Crud");
         }
         public async Task<IActionResult> UpdateCar(GeneralDataViewModel model)
         {
-            using (dbContext)
-            {
-                int selectedValueCar = Convert.ToInt32(Request.Form["ddl_cars"]);
-                int selectedValueDriver = Convert.ToInt32(Request.Form["ddl_drivers"]);
-                List<Car> dbRef_Cars = dbContext.Cars.ToList();
-
-                foreach (Car item in dbRef_Cars)
-                {
-                    if(item.ID == selectedValueCar)
-                    {
-                        item.name = model.newCar.name;
-                        item.number = model.newCar.number;
-                        item.currentDriver = selectedValueDriver;
-                        dbContext.Cars.Update(item);
-                        await dbContext.SaveChangesAsync();
-                        
-                    }
-                }
-                return RedirectToAction("Crud");
-            }
+            int selectedValueCar = Convert.ToInt32(Request.Form["ddl_cars"]);
+            carService.UpdateCar(selectedValueCar,model.newCar.number,model.newCar.name);
+            return RedirectToAction("Crud");
         }
         public async Task<IActionResult> UpdateLap(GeneralDataViewModel model)
         {
-            using (dbContext)
-            {
-                int selectedValueLap = Convert.ToInt32(Request.Form["ddl_laps"]);
-                int selectedValueCar = Convert.ToInt32(Request.Form["ddl_cars"]);
-                int selectedValueDriver = Convert.ToInt32(Request.Form["ddl_drivers"]);
-                List<Lap> dbRef_Laps = dbContext.Laps.ToList();
-
-                foreach (Lap item in dbRef_Laps)
-                {
-                    if(item.ID == selectedValueLap)
-                    {
-                        
-                        item.recordedDriverID = selectedValueDriver;
-                        item.carID = selectedValueCar;
-                        item.lapNumber = model.newLap.lapNumber;
-                        item.recordedLapTime = model.newLap.recordedLapTime;
-                        dbContext.Laps.Update(item);
-                        await dbContext.SaveChangesAsync();
-                        
-                    }
-                }
-                return RedirectToAction("Crud");
-            }
+            int selectedValueLap = Convert.ToInt32(Request.Form["ddl_laps"]);
+            lapService.UpdateLap(selectedValueLap,model.newLap.lapNumber);
+            return RedirectToAction("Crud");
         }
-        
-        
+        public async Task<IActionResult> UpdateRecord(GeneralDataViewModel model)
+        {
+            int selectedValueRecord = Convert.ToInt32(Request.Form["ddl_records"]);
+            int selectedValueCar = Convert.ToInt32(Request.Form["ddl_cars"]);
+            int selectedValueDriver = Convert.ToInt32(Request.Form["ddl_drivers"]);
+            recService.UpdateRecord(selectedValueRecord,UtilitysRef.ConvertTimeInputToSeconds(model.timeIN)
+            ,selectedValueDriver,selectedValueCar,model.newLap.lapNumber);
 
-    #endregion
-    #region Delete
+            return RedirectToAction("Crud");
+        }
+#endregion
+#region Delete
     public async Task<IActionResult> DeleteData(GeneralDataViewModel model)
         {
             string command = "";
@@ -321,40 +171,19 @@ public class HomeController : Controller
                 switch(command){
                     case "DRIVER":
                         deleteID = Convert.ToInt32(Request.Form["ddl_driver"]);
-                        foreach(Driver item in dbContext.Drivers.ToList())
-                        {
-                            if(item.ID == deleteID)
-                            {
-                                item.active = false;
-                                dbContext.Drivers.Update(item);
-                                break;
-                            }
-                        }
-                        
+                        driverSerivce.DeleteDriver(deleteID);
                     break;
                     case "CAR":
                         deleteID = Convert.ToInt32(Request.Form["ddl_car"]);
-                        foreach(Car item in dbContext.Cars.ToList())
-                        {
-                            if(item.ID == deleteID)
-                            {
-                                item.active = false;
-                                dbContext.Cars.Update(item);
-                                break;
-                            }
-                        }
+                        carService.DeleteCar(deleteID);
                     break;
-
                     case "LAP":
                         deleteID = Convert.ToInt32(Request.Form["ddl_lap"]);
-                        foreach(Lap item in dbContext.Laps.ToList())
-                        {
-                            if(item.ID == deleteID)
-                            {
-                                dbContext.Laps.Remove(item);
-                                break;
-                            }
-                        }
+                        lapService.DeleteLap(deleteID);
+                    break;
+                    case "RECORD":
+                        deleteID = Convert.ToInt32(Request.Form["ddl_record"]);
+                        recService.DeleteRecord(deleteID);
                     break;
                     
                 }
@@ -362,8 +191,7 @@ public class HomeController : Controller
             }
             return RedirectToAction("Crud");
         }
-    
-    #endregion
+#endregion
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
---
+++ Migrations/20220614231125_rebuild2.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lapGen.Migrations
+{
+    public partial class rebuild2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    active = table.Column<bool>(type: "INTEGER", nullable: false),
+                    number = table.Column<int>(type: "INTEGER", nullable: false),
+                    name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Drivers",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    active = table.Column<bool>(type: "INTEGER", nullable: false),
+                    name = table.Column<string>(type: "TEXT", nullable: true),
+                    rank = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drivers", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Laps",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    lapNumber = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Laps", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Records",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    timeInSeconds = table.Column<int>(type: "INTEGER", nullable: false),
+                    carID = table.Column<int>(type: "INTEGER", nullable: false),
+                    driverID = table.Column<int>(type: "INTEGER", nullable: false),
+                    lapID = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Records", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Records_Cars_carID",
+                        column: x => x.carID,
+                        principalTable: "Cars",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Records_Drivers_driverID",
+                        column: x => x.driverID,
+                        principalTable: "Drivers",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Records_Laps_lapID",
+                        column: x => x.lapID,
+                        principalTable: "Laps",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Records_carID",
+                table: "Records",
+                column: "carID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Records_driverID",
+                table: "Records",
+                column: "driverID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Records_lapID",
+                table: "Records",
+                column: "lapID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Records");
+
+            migrationBuilder.DropTable(
+                name: "Cars");
+
+            migrationBuilder.DropTable(
+                name: "Drivers");
+
+            migrationBuilder.DropTable(
+                name: "Laps");
+        }
+    }
+}
